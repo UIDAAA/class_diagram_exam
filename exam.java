83e58529550f1dcb26dd09be5f50a611d4d4b566
uida <uidaaq@gmail.com>
2026-04-09 15:45:48 +0900

    1

--- exam.java
+++ exam.java
@@ -1,1 +1,21 @@
+public class Potion {
+}
+public class Weapon {
+}
+public class Monster {
+}
+public class Character {
+    public Potion myPotion = new Potion();
+    public Weapon myWeapon
 
+    public Character() {
+    }
+
+    public void getWeapon(Weapon weapon) {
+        this.myWeapon = weapon;
+
+    }
+
+    public void use(Monster Monster) {
+    }
+}
